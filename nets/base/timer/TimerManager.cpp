2cf6a7b00e9972ee9a782ba9f897f6df2117dc46
guang19 <2196927727@qq.com>
2022-08-26 00:43:19 +0800

    add TimerManager

---
+++ nets/base/timer/TimerManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by guang19
+//
+
+#include "nets/base/timer/TimerManager.h"
---
+++ nets/base/timer/TimerManager.h
@@ -0,0 +1,17 @@
+//
+// Created by guang19
+//
+
+#ifndef NETS_BASE_TIMER_MANAGER_H
+#define NETS_BASE_TIMER_MANAGER_H
+
+#include "nets/base/timer/Timer.h"
+
+namespace nets::base
+{
+    class TimerManager : nets::base::Noncopyable
+    {
+    };
+} // namespace nets::base
+
+#endif // NETS_BASE_TIMER_MANAGER_H
